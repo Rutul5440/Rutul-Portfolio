efe859f87bd439f026ed22372d61486ddf401e82
Rutul5440 <rutulsuthar2018@gmail.com>
2026-04-02 00:14:18 +0530

    added base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Rutul-Portfolio/",
   server: {
     host: "::",
     port: 8080,
